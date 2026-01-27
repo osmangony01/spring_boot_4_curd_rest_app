0f6ab611d0fded87b0742922fea4a1f30f6e9afe
Md Osman Gony <mdosmangoni376@gmail.com>
2026-01-27 20:13:15 +0600

    project setup for rest api

---
+++ src/test/java/org/example/spring_boot_curd_rest_app/SpringBootCurdRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_boot_curd_rest_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCurdRestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
